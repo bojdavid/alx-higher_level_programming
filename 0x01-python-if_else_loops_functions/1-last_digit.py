92c72cec92e7a90825cc1ef3f1dd95f8df9df980
bojdavid <bojdavid80@gmail.com>
2023-02-17 23:14:58 +0100

    chceking

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,17 +3,17 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-	sign = -1
+    sign = -1
 else:
-	sign = 1
+    sign = 1
 num = number * sign
 a = str(num)
 for i in a:
-	last_digit = int(i) * sign
+    last_digit = int(i) * sign
 
 if last_digit > 5:
-	print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-	print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-	print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-	a = ""
-	for i in str(number):
-		a = i
-	print(int(a), end='')
-	return(int(a))
+    a = ""
+    for i in str(number):
+        a = i
+    print(int(a), end='')
+    return(int(a))
