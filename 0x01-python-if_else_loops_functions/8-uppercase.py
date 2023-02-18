7f7a7ad14ba39c1d2584d998c165deb54cf35f10
bojdavid <bojdavid80@gmail.com>
2023-02-18 10:45:21 +0100

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(97, 122):
+            up = ord(i) - 32
+            print('{}'.format(chr(up)), end='')
+        else:
+            print('{}'.format(i), end='')
+    print('')
