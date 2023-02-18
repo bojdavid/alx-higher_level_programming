5ba92135ff5d0363cfd13526d94e3482f83f216b
bojdavid <bojdavid80@gmail.com>
2023-02-18 09:56:39 +0100

    message

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for i in range (97,123):
-        if c != chr(i):
-            for j in range(65, 91):
-                if c == chr(j):
-                    a = False
-                    break
-        else:
-            a = True
-    return a
+    if type(c) == str:
+        for i in range (97,123):
+            if c != chr(i):
+                        a = False
+            else:
+                a = True
+                break
+        return a
+    else:
+        raise Error("value is not a string")
