92e2fada1fee0d1dad761af297284ad05ce4ab3f
bojdavid <bojdavid80@gmail.com>
2023-02-20 10:41:37 +0100

    msg

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
-        print('')
+    if my_list is None:
         return
     count = len(my_list) - 1
     for i in my_list:
