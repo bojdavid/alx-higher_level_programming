0215938eacf22136a403de7b8796b47e7167c88c
bojdavid <bojdavid80@gmail.com>
2023-02-20 11:20:37 +0100

    msg

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,7 +11,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b = 0 + tuple_a[1]
     elif len(tuple_a) == 0:
         a = 0 + tuple_b[0]
-        b = 0 + tuple_a[1]
+        b = 0 + tuple_b[1]
     elif len(tuple_b) >= 2:
         a = tuple_a[0] + tuple_b[0]
         b = tuple_b[1] + tuple_a[1]
