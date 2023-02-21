f7207622d2e51c9537ad95f9bf31864dd8518f6f
bojdavid <bojdavid80@gmail.com>
2023-02-21 13:34:52 +0100

    msg

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(0, x):
+            if type(my_list[i]) == int:
+                print('{:d}'.format(my_list[i]), end='')
+                count += 1
+            else:
+                continue
+        print("")
+        return count
+    except Exception as e:
+        print(e)
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
