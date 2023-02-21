e1b3db070c080fdc62a350ee199010c58f4f035e
bojdavid <bojdavid80@gmail.com>
2023-02-21 14:40:44 +0100

    msg

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,8 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-        except Exception:
+        except Exception as e:
+            print(e)
             div = 0
         finally:
             a.append(div)
