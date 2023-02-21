80e010497b6d64b1d2d70667208f60512bd97e73
bojdavid <bojdavid80@gmail.com>
2023-02-21 16:55:23 +0100

    msg

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This is module documentation
+"""
+
 class Square:
+    """
+     Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        msg (str): Human readable string describing the exception.
+        code (:obj:`int`, optional): Error code.
+
+    Attributes:
+        msg (str): Human readable string describing the exception.
+        code (int): Exception error code.
+    """
     pass
