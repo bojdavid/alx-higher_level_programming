b28cde7adef97438252d66310f375c70ab1c7245
bojdavid <bojdavid80@gmail.com>
2023-02-21 17:53:00 +0100

    msg

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Write a class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+"""
+
+
+class Square:
+    """
+        class documentation
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+            returns the current square area
+        """
+        area = self.__size * self.__size
+        return area
